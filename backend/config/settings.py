8bb2e185fae9bb79acf0599d8532eba17fdeba4d
Oscar <jan.oscar.backman@gmail.com>
2026-01-12 14:50:15 +0100

    Auth & Database foundation (JWT, dj-rest-auth, Postgres)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "allauth.account.middleware.AccountMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
