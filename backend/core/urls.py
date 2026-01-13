b9103bcb6c6ba5a03bda0121f76386f4bcf9fad9
Oscar <jan.oscar.backman@gmail.com>
2026-01-14 00:04:15 +0100

    Add job postings and employer domain models

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import JobApplicationViewSet, me
+from .views import JobApplicationViewSet, JobPostingViewSet, me
 
 router = DefaultRouter()
-router.register("applications", JobApplicationViewSet, basename="applications")
+router.register(r"applications", JobApplicationViewSet, basename="applications")
+router.register(r"postings", JobPostingViewSet, basename="postings")
 
 urlpatterns = [
     path("me/", me, name="me"),
